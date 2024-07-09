7ce838c7a29a1341a580db1a0591a5e3634bee87
Kane Deiley <kane.deiley@sluhn.org>
2024-07-09 13:57:09 -0400

    update logo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import "./App.css";
 import logo from "./logo.svg";
 
 function App() {
-  return;
-  <div className="cont">
-    <img src={logo} className="App-logo" alt="logo" />
-  </div>;
+  return (
+    <div className="cont">
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
 }
 
 export default App;
