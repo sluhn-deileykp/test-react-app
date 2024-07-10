97f941d20d5f1c270e9860df991af23d37fc1dd0
Kane Deiley <kane.deiley@sluhn.org>
2024-07-10 15:16:05 -0400

    Add weather API with env

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+import Header from "./Components/Header";
+import axios from "axios";
+
+function App() {
+  const [location, setLocation] = useState(null);
+  const [temp, setTemp] = useState(null);
+
+  async function findTemp() {
+    let url =
+      "http://api.weatherapi.com/v1/current.json?key=" +
+      process.env.REACT_APP_WEATHER_API +
+      "&q=" +
+      location +
+      "&aqi=no";
+    let weatherData = null;
+    if (location) {
+      try {
+        const response = await axios.get(url);
+        weatherData = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+      if (weatherData) {
+        setTemp(weatherData.current.temp_f);
+      } else {
+        setTemp("--");
+      }
+    }
+  }
+
+  return (
+    <>
+      <Header></Header>
+      <div className="cont">
+        <img src={logo} className="App-logo" alt="logo" />
+        <input
+          className="input"
+          placeholder="Enter City for Temp"
+          onChange={(e) => {
+            setLocation(e.target.value);
+          }}
+        ></input>
+        <button
+          className="input"
+          onClick={() => {
+            findTemp();
+          }}
+        >
+          Find
+        </button>
+        <span className="temp">{temp}°F</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
